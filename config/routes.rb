174dcf705907f6bd355a3a633cbfc152735f91a4
Swanand Pethe <spethe@equalexperts.com>
2013-07-19 16:25:09 +0530

    included route

--- config/routes.rb
+++ config/routes.rb
@@ -7,11 +7,11 @@ DemoApp::Application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-   root 'users#index'
+  # root 'users#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
-
+	get '/users', to: 'users#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
