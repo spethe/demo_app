bf3b02d3176d536a7f399e8501cf45433c47c516
Swanand Pethe <spethe@equalexperts.com>
2013-07-21 17:59:00 +0530

    Removed map line in routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -12,9 +12,7 @@ DemoApp::Application.routes.draw do
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
-	#	get '/users', to: 'users#index'
-	map.connect 'users', :controller => 'users_controller', :action => 'index'
-
+  #	get '/users', to: 'users#index'
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
 
